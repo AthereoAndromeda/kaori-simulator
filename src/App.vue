8e3b9e0f3e15a9d65fb0d7821149ecb83be39476
Angelo <athereoandromeda@gmail.com>
2021-11-22 13:12:56 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import KS from "./components/KaoriSimulator.vue";
+</script>
+
+<template>
+  <KS />
+
+  <footer>
+    <p>Fork me on <a href="">Github</a></p>
+    Powered by <a href="https://icanhazdadjoke.com">icanhazdadjoke API</a>
+  </footer>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
